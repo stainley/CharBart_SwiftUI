931658dd03bfecfbc92e3f7d11ca09807e93585c
Stainley Lebron <stainley.lebron@gmail.com>
2023-09-05 13:40:48 -0400

    BartChar

--- BarChartApp/ContentView.swift
+++ BarChartApp/ContentView.swift
@@ -6,14 +6,82 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
+    
+    let viewMonths: [ViewMonth] = [
+        .init(date: Date.from(year: 2023, month: 1, day: 1), viewCount: 55000),
+        .init(date: Date.from(year: 2023, month: 2, day: 1), viewCount: 89000),
+        .init(date: Date.from(year: 2023, month: 3, day: 1), viewCount: 64000),
+        .init(date: Date.from(year: 2023, month: 4, day: 1), viewCount: 79000),
+        .init(date: Date.from(year: 2023, month: 5, day: 1), viewCount: 130000),
+        .init(date: Date.from(year: 2023, month: 6, day: 1), viewCount: 90000),
+        .init(date: Date.from(year: 2023, month: 7, day: 1), viewCount: 88000),
+        .init(date: Date.from(year: 2023, month: 8, day: 1), viewCount: 64000),
+        .init(date: Date.from(year: 2023, month: 9, day: 1), viewCount: 74000),
+        .init(date: Date.from(year: 2023, month: 10, day: 1), viewCount: 99000),
+        .init(date: Date.from(year: 2023, month: 11, day: 1), viewCount: 110000),
+        .init(date: Date.from(year: 2023, month: 12, day: 1), viewCount: 94000),
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 4) {
+            Text("YouTube Views")
+            
+            Text("Total: \(viewMonths.reduce(0, { $0 + $1.viewCount}))")
+                .fontWeight(.semibold)
+                .font(.footnote)
+                .foregroundColor(.secondary)
+                .padding(.bottom, 12)
+            
+            Chart {
+                RuleMark(y: .value("Goal", 80000))
+                    .foregroundStyle(Color.mint)
+                    .lineStyle(StrokeStyle(lineWidth: 1, dash: [5]))
+                    /*.annotation(alignment: .leading) {
+                        Text("Goal")
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }*/
+                ForEach(viewMonths) { viewMonth in
+                    BarMark(
+                        x: .value("Month", viewMonth.date, unit: .month),
+                        y: .value("Views", viewMonth.viewCount)
+                    )
+                    .foregroundStyle(Color.pink)
+                }
+            }
+            .frame(height: 180)
+            .chartXAxis(content: {
+                AxisMarks(values: viewMonths.map {$0.date}) {date in
+                    AxisGridLine()
+                    AxisTick()
+                    AxisValueLabel(format: .dateTime.month(.narrow))
+                }
+            })
+            .chartYAxis(content: {
+                AxisMarks()
+            })
+            //.chartYScale(domain: 0...200000)
+            //.chartXAxis(.hidden)
+            //.chartYAxis(.hidden)
+            .chartPlotStyle { plotContent in
+                plotContent
+                    .background(.black.opacity(0.3))
+                    .border(.green, width: 3)
+            }
+            .padding(.bottom)
+            
+            HStack {
+                Image(systemName: "line.diagonal")
+                    .rotationEffect(Angle(degrees: 45))
+                    .foregroundColor(.secondary)
+                
+                Text("Monthly goal")
+                    .foregroundColor(.secondary)
+            }
+            
         }
         .padding()
     }
@@ -22,5 +90,21 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ViewMonth: Identifiable {
+    let id = UUID()
+    let date: Date // X axis
+    let viewCount: Int // Y axis
+    
+}
+
+
+extension Date {
+    static func from(year: Int, month: Int, day: Int) -> Date {
+        let components = DateComponents(year: year, month: month, day: day)
+        
+        return Calendar.current.date(from: components)!
     }
 }
