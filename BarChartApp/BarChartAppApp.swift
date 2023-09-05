6e72e0e61ee346bc5a9f1ad4c49be50813c284bd
Stainley Lebron <stainley.lebron@gmail.com>
2023-09-05 12:56:55 -0400

    Initial Commit

---
+++ BarChartApp/BarChartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartAppApp.swift
+//  BarChartApp
+//
+//  Created by Stainley A Lebron R on 2023-09-05.
+//
+
+import SwiftUI
+
+@main
+struct BarChartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarChartApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BarChartApp
+//
+//  Created by Stainley A Lebron R on 2023-09-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
